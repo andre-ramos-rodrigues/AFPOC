f80e8bbcf4c56e60c5dfc43bb0b5bfc685dd030a
Andre <andreramos@id.uff.br>
2022-09-12 13:30:07 -0300

    .

--- controllers/getTunels.js
+++ controllers/getTunels.js
@@ -3,7 +3,7 @@ import Tunel from '../models/tunel.js'
 export const getTunels = async(req,res) => {
   try {
     const Tunels = await Tunel.find().sort({ createdAt: -1})
-    res.set('Access-Control-Allow-Origin', 'https://631f5bfaf482183823aa8ed2--cute-unicorn-84762b.netlify.app/');
+    res.set('Access-Control-Allow-Origin', 'https://cute-unicorn-84762b.netlify.app');
     res.status(200).json(Tunels)
   } catch(err) {
     res.status(500).send(err)
